90f9e4177d64f80f63ee838cd8339ab204863c84
jetscooters <david.silva.25812@gmail.com>
2022-03-13 18:34:10 -0500

    Rq

--- backend/__test__/backend.test.js
+++ backend/__test__/backend.test.js
@@ -2,6 +2,7 @@ const request = require("supertest");
 const app = require('../routes/index');
 describe('Testing Backend Connections', () =>{
     test('Testing root Connection',() => {
+        console.log("Requesting routes\n");
         return request(app).get("/").expect(200);
     });
 });
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 router = express.Router();
 index = require("../controllers/index");
 
+console.log("About to call controllers\n");
+
 router.get('/', index.index);
 
 module.exports = router;
