9dccb33ea9fa4b2245d16fe825dc24695c9eec42
leecavazos <cavazoslee@gmail.com>
2022-03-13 18:48:33 -0500

    Adding code to Backend

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app= express();
 const cors = require('cors');
+const data = require('./routes/QuoteHistoryData');
 const Pricing_mod = require('./classes/PricingModule');
 
 app.use(express.json({ limit: '1mb'}));
 app.use(express.static("public"));
 app.use(cors());
 app.use('/', require('./routes/index'));
+
+app.get("/QuoteHistory", (req, res) => {
+    res.json(data)
+});
 
 //----POST REQUEST  for Registration Form ----//
 app.post('/api/Registration', (req, res) => {
